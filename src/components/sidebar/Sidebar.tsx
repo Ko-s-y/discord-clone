b038e9407c42b1db92d653bcd54de6545c9a120b
s-ko <song.1209@icloud.com>
2023-12-31 00:48:45 +0900

    chore: set servericon

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -3,7 +3,24 @@ import "./Sidebar.scss"
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className='sidebar'>
+      {/* {sidebarLeft} */}
+      <div className='sidebarLeft'>
+        <div className='serverIcon'>
+          <img src='./logo192.png' alt='' />
+        </div>
+        <div className='serverIcon'>
+          <img src='./logo192.png' alt='' />
+        </div>
+      </div>
+
+      {/* {sidebarRight} */}
+      <div className='sidebarRight'>
+        <div className='sidebarTop'>
+          <h3>Discord</h3>
+        </div>
+      </div>
+    </div>
   )
 }
 
