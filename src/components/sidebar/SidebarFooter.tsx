3ac66cb3f3f45da16d079c35bbcedfcfb728dbf3
s-ko <song.1209@icloud.com>
2023-12-31 14:08:05 +0900

    add: sidebarFooter

--- src/components/sidebar/SidebarFooter.tsx
+++ src/components/sidebar/SidebarFooter.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import "./SidebarFooter.scss"
+import MicIcon from '@mui/icons-material/Mic';
+import HeadphonesIcon from '@mui/icons-material/Headphones';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 function SidebarFooter() {
   return (
-    <div>SidebarFooter</div>
+    <div className='sidebarFooter'>
+      <div className='sidebarAccount'>
+        <img src='./icon.png' alt='' />
+        <div className='accountName'>
+          <h4>Ko-s-y</h4>
+          <span>#8888</span>
+        </div>
+      </div>
+      <div className='sidebarVoice'>
+        <MicIcon />
+        <HeadphonesIcon />
+        <SettingsIcon />
+      </div>
+    </div>
   )
 }
 
