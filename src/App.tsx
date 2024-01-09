b082ee038158c5ab1f6ea96b918ed9e5d316f07a
s-ko <song.1209@icloud.com>
2024-01-09 23:18:29 +0900

    add: auth, dispalys paramms

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.scss';
 import { useSelector } from 'react-redux';
 import Sidebar from './components/sidebar/Sidebar';
 import Chat from './components/chat/Chat';
 import Login from './components/login/Login';
 import { useAppSelector } from './app/hooks';
+import { useDispatch } from 'react-redux';
+import { auth } from './firebase';
 
 function App() {
   const user = useAppSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((loginUser) => {
+      console.log(loginUser);
+    });
+  }, []);
 
   return (
     <div className="App">
