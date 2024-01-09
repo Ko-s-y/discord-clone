f1de5bf649e0564cbdec808f08c2aba0e88c0ae2
s-ko <song.1209@icloud.com>
2024-01-10 00:35:01 +0900

    add: dispatch user

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Login from './components/login/Login';
 import { useAppSelector } from './app/hooks';
 import { useDispatch } from 'react-redux';
 import { auth } from './firebase';
+import { login, logout } from './features/userSlice';
 
 function App() {
   const user = useAppSelector((state) => state.user);
@@ -15,8 +16,18 @@ function App() {
   useEffect(() => {
     auth.onAuthStateChanged((loginUser) => {
       console.log(loginUser);
+      if (loginUser) {
+        dispatch(login({
+          uid: loginUser.uid,
+          photo: loginUser.photoURL,
+          email: loginUser.email,
+          displayName: loginUser.displayName,
+        }));
+      } else {
+        dispatch(logout());
+      }
     });
-  }, []);
+  }, [dispatch]);
 
   return (
     <div className="App">
