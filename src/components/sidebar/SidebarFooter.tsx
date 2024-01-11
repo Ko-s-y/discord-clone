be0a1f34d4f39073e0070971621043cfa5f8994d
s-ko <song.1209@icloud.com>
2024-01-11 23:32:20 +0900

    add: signOut

--- src/components/sidebar/SidebarFooter.tsx
+++ src/components/sidebar/SidebarFooter.tsx
@@ -3,12 +3,13 @@ import "./SidebarFooter.scss"
 import MicIcon from '@mui/icons-material/Mic';
 import HeadphonesIcon from '@mui/icons-material/Headphones';
 import SettingsIcon from '@mui/icons-material/Settings';
+import { auth } from '../../firebase';
 
 function SidebarFooter() {
   return (
     <div className='sidebarFooter'>
       <div className='sidebarAccount'>
-        <img src='./icon.png' alt='' />
+        <img src='./icon.png' alt='' onClick={() => auth.signOut()} />
         <div className='accountName'>
           <h4>Ko-s-y</h4>
           <span>#8888</span>
