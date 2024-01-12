4fd5c38ecac3d82b947985ea31dcbd0dd2005570
s-ko <song.1209@icloud.com>
2024-01-12 23:52:47 +0900

    chore: displays loginUser infos

--- src/components/sidebar/SidebarFooter.tsx
+++ src/components/sidebar/SidebarFooter.tsx
@@ -4,15 +4,18 @@ import MicIcon from '@mui/icons-material/Mic';
 import HeadphonesIcon from '@mui/icons-material/Headphones';
 import SettingsIcon from '@mui/icons-material/Settings';
 import { auth } from '../../firebase';
+import { useAppSelector } from '../../app/hooks';
 
 function SidebarFooter() {
+  const user = useAppSelector((state) => state.user);
+
   return (
     <div className='sidebarFooter'>
       <div className='sidebarAccount'>
-        <img src='./icon.png' alt='' onClick={() => auth.signOut()} />
+        <img src={ user?.photo } alt='' onClick={() => auth.signOut()} />
         <div className='accountName'>
-          <h4>Ko-s-y</h4>
-          <span>#8888</span>
+          <h4>{ user?.displayName }</h4>
+          <span>#{ user?.uid.substring(0, 4) }</span>
         </div>
       </div>
       <div className='sidebarVoice'>
